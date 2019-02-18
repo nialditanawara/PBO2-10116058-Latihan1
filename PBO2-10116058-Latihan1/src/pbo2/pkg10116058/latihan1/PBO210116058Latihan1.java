eaec837dbbefd903640b70217be2307ab41ffca5
nialditanawara <31877365+nialditanawara@users.noreply.github.com>
2019-02-19 01:19:46 +0700

    Add files via upload

---
+++ PBO2-10116058-Latihan1/src/pbo2/pkg10116058/latihan1/PBO210116058Latihan1.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10116058.latihan1;
+
+/**
+ *
+ * @author xider
+ * NAMA  : Nialdi Tanawara
+ * KELAS : PBO2
+ * NIM   : 10116058
+ * Deskripsi Program : Perintah Menampilkan Value Ke Layar
+ */
+public class PBO210116058Latihan1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         System.out.println("BIODATA ROCKSTAR" + "\n" + "============="
+                + "====================");
+        System.out.println("NAMA\t\t: Nialdi Tanawara");
+        System.out.println("ALAMAT\t\t: Cikutra Barat");
+        System.out.println("TEMPAT LAHIR\t: Pati");
+        System.out.println("TANGGAL LAHIR\t: 13 April 1998");
+        System.out.println("=================================");
+        System.out.println("HOBI\t\t: Bermusik");
+        System.out.println("MAKES\t\t: Soto");
+        System.out.println("MIKES\t\t: Cendol"); 
+    }
+    
+}
